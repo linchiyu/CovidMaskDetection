089fe46a1e9b763bfb38de64d3c743755336b714
Lin Chi Yu <chiyupc+linchiyu@users.noreply.github.com>
2020-11-08 14:30:54 -0300

    Update iocontroller.py

--- utils/iocontroller.py
+++ utils/iocontroller.py
@@ -14,10 +14,19 @@ class IoManager():
         #catraca - OUT
         #saída de sinal LOW libera a catraca
         self.catracaDireita = 23
-        self.catracaEsquerda = 25
+        self.catracaEsquerda = 24
         #contagem catraca - IN
         #VERIFICAR REGRA DO SINAL
-        self.contagemCatraca = 24
+        #self.contagemCatraca = 24
+        self.sensorEsqCat = 5
+        self.sensorDirCat = 6
+
+        self.sensorEsqCat2 = 18
+        self.sensorDirCat2 = 25
+
+        #numero de pessoas dentro da loja
+        self.contagem = 0
+
         #temperatura - IN
         #entrada de sinal LOW significa ativacao do sensor de temperatura
         #1 LOW = temperatura normal
@@ -28,6 +37,7 @@ class IoManager():
         self.sensorAlcool = 17
         self.alcoolVazio = 27
 
+
         if 'nt' in os.name: #windows
             self.has_GPIO = False
         else:
@@ -36,8 +46,13 @@ class IoManager():
             
             GPIO.setup(self.catracaDireita, GPIO.OUT)
             GPIO.setup(self.catracaEsquerda, GPIO.OUT)
+
+            GPIO.setup(self.sensorEsqCat, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.setup(self.sensorDirCat, GPIO.IN, pull_up_down=GPIO.PUD_UP)
             
-            GPIO.setup(self.contagemCatraca, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            #GPIO.setup(self.contagemCatraca, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.setup(self.sensorEsqCat2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.setup(self.sensorDirCat2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
             #GPIO.add_event_detect(self.contagemCatraca, GPIO.RISING, bouncetime=300)
 
             GPIO.setup(self.temperatura, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -80,29 +95,31 @@ class IoManager():
         
     def avaliarTemperatura(self):
         if self.has_GPIO:
-            x = GPIO.wait_for_edge(self.temperatura, GPIO.RISING, timeout=TIME_TEMP*1000)
-            if x == None:
-                pass
-            else:
-                x = GPIO.wait_for_edge(self.temperatura, GPIO.FALLING, timeout=300)
+            now = time.time()
+            while time.time() - now <= TIME_TEMP*1000:
+                x = GPIO.wait_for_edge(self.temperatura, GPIO.RISING, timeout=250)
                 if x == None:
-                    self.outputQ.put('pass')
-                    #print('pass')
-                    self.step = 0
+                    pass
                 else:
-                    GPIO.wait_for_edge(self.temperatura, GPIO.RISING, timeout=500)
-                    self.outputQ.put('stop')
-                    #print('stop')
+                    x = GPIO.wait_for_edge(self.temperatura, GPIO.FALLING, timeout=300)
+                    if x == None:
+                        self.outputQ.put('pass')
+                        print('pass')
+                        self.step = 0
+                    else:
+                        GPIO.wait_for_edge(self.temperatura, GPIO.RISING, timeout=500)
+                        self.outputQ.put('stop')
+                        print('stop')
+                    break
         else:
             print('avaliando temperatura GPIO')
             self.outputQ.put('pass')
             self.step = 0
 
     def alcoolSignal(self, channel):
-        '''if self.step == 3:
+        if self.step == 3:
             self.outputAQ.put('pass')
-            self.step = 0'''
-        self.outputAQ.put('pass')
+            self.step = 0
         
     def avaliarAlcool(self):
         if self.has_GPIO:
@@ -114,24 +131,43 @@ class IoManager():
 
     def liberarCatraca(self):
         ##############
+        if self.contagem >= CAPACIDADE_PESSOAS:
+            #capacidade máxima de pessoas atingida
+            self.outputQ.put('stop')
+            print('stop')
         #adicionar logica de pessoa ja ter passado na catraca
-        time.sleep(1)
-        self.setLow(self.catracaDireita)
-        self.setLow(self.catracaEsquerda)
-        time.sleep(1)
-        self.setHigh(self.catracaDireita)
-        self.setHigh(self.catracaEsquerda)
-        self.step = 0
-        if not self.has_GPIO:
+        if self.has_GPIO:
+            time.sleep(1)
+            self.setLow(self.catracaDireita)
+            self.setLow(self.catracaEsquerda)
+            time.sleep(1)
+            self.setHigh(self.catracaDireita)
+            self.setHigh(self.catracaEsquerda)
+            self.step = 0
+        else:
+            print('catraca liberada')
+
+    def passagemCatraca(self):
+        ##############
+        #adicionar logica de pessoa ja ter passado na catraca
+        if self.has_GPIO:
+            time.sleep(1)
+            self.setLow(self.catracaDireita)
+            self.setLow(self.catracaEsquerda)
+            time.sleep(1)
+            self.setHigh(self.catracaDireita)
+            self.setHigh(self.catracaEsquerda)
+            self.step = 0
+        else:
             print('catraca liberada')
 
     def loopGpio(self):
         while True:
             if self.step == 1:
                 self.avaliarTemperatura()
-                '''elif self.step == 3:
-                self.avaliarAlcool()
-                pass'''
+            elif self.step == 3:
+                #self.avaliarAlcool()
+                pass
                 '''elif self.step == 5:
                 self.liberarCatraca()'''
             else:
