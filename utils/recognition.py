d3eb22d13d0a7648389075c6145d91885e64477c
Lin Chi Yu <chiyupc+linchiyu@users.noreply.github.com>
2020-12-30 15:33:45 -0300

    testing new face detector

--- utils/recognition.py
+++ utils/recognition.py
@@ -5,15 +5,57 @@ import numpy as np
 import time
 from multiprocessing import Process, Queue
 from .api_request import API
-from . import Facenet
 from . import distance as dst
-import sys
-import imutils
-import imutils.face_utils
-import dlib
-from keras.preprocessing import image
-import keras
-from apscheduler.schedulers.background import BackgroundScheduler
+import pandas as pd
+from PIL import Image
+
+
+def alignment_procedure(img, left_eye, right_eye):   
+    #this function aligns given face in img based on left and right eye coordinates
+    
+    left_eye_x, left_eye_y = left_eye
+    right_eye_x, right_eye_y = right_eye
+    
+    #-----------------------
+    #find rotation direction
+        
+    if left_eye_y > right_eye_y:
+        point_3rd = (right_eye_x, left_eye_y)
+        direction = -1 #rotate same direction to clock
+    else:
+        point_3rd = (left_eye_x, right_eye_y)
+        direction = 1 #rotate inverse direction of clock
+    
+    #-----------------------
+    #find length of triangle edges
+    
+    a = dst.findEuclideanDistance(np.array(left_eye), np.array(point_3rd))
+    b = dst.findEuclideanDistance(np.array(right_eye), np.array(point_3rd))
+    c = dst.findEuclideanDistance(np.array(right_eye), np.array(left_eye))
+    
+    #-----------------------
+    
+    #apply cosine rule
+            
+    if b != 0 and c != 0: #this multiplication causes division by zero in cos_a calculation
+        
+        cos_a = (b*b + c*c - a*a)/(2*b*c)
+        angle = np.arccos(cos_a) #angle in radian
+        angle = (angle * 180) / math.pi #radian to degree
+        
+        #-----------------------
+        #rotate base image
+        
+        if direction == -1:
+            angle = 90 - angle
+        
+        img = Image.fromarray(img)
+        img = np.array(img.rotate(direction * angle))
+    
+    #-----------------------
+    
+    return img #return img anyway
+
 
 class FaceRecog():
     """face recognition from face_recognition"""
@@ -78,15 +120,75 @@ class FaceRecog():
                     largest_size = size
         return largest_size, largest
 
+    def align_face(self, img):
+        detected_face_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #eye detector expects gray scale image
+        eyes = self.eye_detector.detectMultiScale(detected_face_gray)
+        
+        if len(eyes) >= 2:
+            #find the largest 2 eye
+            base_eyes = eyes[:, 2]
+            
+            items = []
+            for i in range(0, len(base_eyes)):
+                item = (base_eyes[i], i)
+                items.append(item)
+            
+            df = pd.DataFrame(items, columns = ["length", "idx"]).sort_values(by=['length'], ascending=False)
+            
+            eyes = eyes[df.idx.values[0:2]] #eyes variable stores the largest 2 eye
+            
+            #-----------------------
+            #decide left and right eye
+            
+            eye_1 = eyes[0]; eye_2 = eyes[1]
+            
+            if eye_1[0] < eye_2[0]:
+                left_eye = eye_1; right_eye = eye_2
+            else:
+                left_eye = eye_2; right_eye = eye_1
+            
+            #-----------------------
+            #find center of eyes
+            
+            left_eye = (int(left_eye[0] + (left_eye[2] / 2)), int(left_eye[1] + (left_eye[3] / 2)))
+            right_eye = (int(right_eye[0] + (right_eye[2]/2)), int(right_eye[1] + (right_eye[3]/2)))
+            
+            img = alignment_procedure(img, left_eye, right_eye)
+            
+        return img #return img anyway
+
     def camInference(self):
-        face_detector = dlib.get_frontal_face_detector()
-        sp = dlib.shape_predictor("./data/weights/shape_predictor_5_face_landmarks.dat")
+        from . import Facenet
+        import sys
+        from keras.preprocessing import image
+        import keras
+        from apscheduler.schedulers.background import BackgroundScheduler
+
+        #load detection
+        face_detector = cv2.dnn.readNetFromCaffe(
+            "./data/weights/deploy.prototxt", 
+            "./data/weights/res10_300x300_ssd_iter_140000.caffemodel"
+        )
+
+        self.eye_detector = cv2.CascadeClassifier("./data/weights/haarcascade_eye.xml")
+
+        ssd_labels = ["img_id", "is_face", "confidence", "left", "top", "right", "bottom"]
+        target_size = (300, 300)
+
+        img_raw = self.imageQ.get(timeout=2)
+        original_size = img_raw.shape
+        aspect_ratio_x = (original_size[1] / target_size[1])
+        aspect_ratio_y = (original_size[0] / target_size[0])
+
+
+        #load recognition
         model = Facenet.loadModel()
         input_shape = model.layers[0].input_shape
         if type(input_shape) == list:
             input_shape = input_shape[0][1:3]
         else:
             input_shape = input_shape[1:3]
+
 
         self.updateFaceList()
         scheduler = BackgroundScheduler()
@@ -106,14 +208,33 @@ class FaceRecog():
                 continue
             faces = []
 
-            detections = face_detector(img_raw)
-            if len(detections) == 0:
-                #nenhuma face detectada
+            img = cv2.resize(img_raw, target_size)
+
+            img = cv2.dnn.blobFromImage(image = img)
+            face_detector.setInput(img)
+            detections = face_detector.forward()
+
+            detections_df = pd.DataFrame(detections[0][0], columns = ssd_labels)
+        
+            detections_df = detections_df[detections_df['is_face'] == 1] #0: background, 1: face
+            detections_df = detections_df[detections_df['confidence'] >= 0.90]
+            
+            detections_df['left'] = (detections_df['left'] * 300).astype(int)
+            detections_df['bottom'] = (detections_df['bottom'] * 300).astype(int)
+            detections_df['right'] = (detections_df['right'] * 300).astype(int)
+            detections_df['top'] = (detections_df['top'] * 300).astype(int)
+
+            if detections_df.shape[0] == 0:
                 self.dataQ.put(self.generate_data())
                 continue
 
-            for d in detections:
-                faces.append(imutils.face_utils.rect_to_bb(d))
+            for instance in detections_df.iloc:
+                x = int(instance["left"]*aspect_ratio_x)
+                y = int(instance["top"]*aspect_ratio_y)
+                w = int(instance["right"]*aspect_ratio_x)-x
+                h = int(instance["bottom"]*aspect_ratio_y)-y
+
+                faces.append((x,y,w,h))
 
             #ao menos uma face detectada, filtrar face principal (maior)
             face_size, face_location = self.extractLargestFace(faces)
@@ -126,10 +247,10 @@ class FaceRecog():
                     continue
 
                 (x, y, w, h) = face_location
-                img_shape = sp(img_raw, dlib.rectangle(x, y, x+w, y+h))
-                face_bgr = dlib.get_face_chip(img_raw, img_shape, size = input_shape[0])
-                face_bgr = cv2.resize(face_bgr, input_shape)
+                face_bgr = img_raw[y:y+h, x:x+w]
+                face_bgr = self.align_face(face_bgr)
 
+                face_bgr = cv2.resize(face_bgr, input_shape)
                 img_pixels = image.img_to_array(face_bgr)
                 img_pixels = np.expand_dims(img_pixels, axis = 0)
                 img_pixels /= 255
